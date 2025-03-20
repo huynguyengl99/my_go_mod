9259d0a0f8fb993f26873fa508e34f8cc47d35de
Huy Nguyen <danghuy1999@gmail.com>
2025-03-20 17:08:36 +0700

    third commit

--- lib.go
+++ lib.go
@@ -1,10 +1,18 @@
 // Package mygomod provides simple math operations.
 package my_go_mod
 
+import (
+	"golang.org/x/exp/constraints"
+)
+
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
 // Add returns the sum of two integers.
 //
 // For more information about addition, visit:
 // https://www.mathsisfun.com/numbers/addition.html
-func Add(a, b int) int {
+func Add[T Number](a, b T) T {
 	return a + b
 }
