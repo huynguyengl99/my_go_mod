2b9e391e48e25ec29588d63a7b214feb79827a86
Huy Nguyen <danghuy1999@gmail.com>
2025-03-20 16:58:26 +0700

    second commit

--- lib.go
+++ lib.go
@@ -1,5 +1,10 @@
+// Package mygomod provides simple math operations.
 package my_go_mod
 
+// Add returns the sum of two integers.
+//
+// For more information about addition, visit:
+// https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
